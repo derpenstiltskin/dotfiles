05e28a8f7886a2c020f96494b64ea3e70856720c
Dustin Riley <dustin@derpenstiltskin.com>
2022-05-18 13:38:34 -0400

    Updated PowerShell dotfiles

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dotfiles
 
-My configs.
+My config files and utility shell scripts.
+
+![It's Candy Mountain](https://raw.githubusercontent.com/derpenstiltskin/dotfiles/main/assets/candymountain.png)
--- windowspowershell/Microsoft.PowerShell_profile.ps1
+++ windowspowershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,8 @@
+# Shell Functions
+function touch { Set-Content -Path ($args[0]) -Value ($null) } # Mimic touch command from *nix
+
 # Module Imports
-Import-Module posh-git
-Import-Module PSReadline
 Import-Module oh-my-posh
 
 # Shell Settings
-Set-PSReadLineOption -PredictionSource History
-Set-PoshPrompt space
-
-# Shell Functions
-function touch { Set-Content -Path ($args[0]) -Value ($null) } 
+oh-my-posh init pwsh --config "${HOME}\AppData\Local\Programs\oh-my-posh\themes\derpenstiltskin.omp.json" | Invoke-Expression
