79c8db3b2d4ade69d45c03de626577f5431677c1
Dustin Riley <dustin@derpenstiltskin.com>
2022-08-20 23:14:29 -0400

    08/20/2022

--- windowspowershell/Microsoft.PowerShell_profile.ps1
+++ windowspowershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,18 @@
 # Shell Functions
+function updateall { winget upgrade --all --force --accept-source-agreements --accept-package-agreements --h }
 function touch { Set-Content -Path ($args[0]) -Value ($null) } # Mimic touch command from *nix
+function .. { Set-Location -Path .. } # Move up a folder
+function .... { Set-Location -Path ..\.. } # Move up two folders
+function ~ { Set-Location -Path ~ } # Move to home folder
 
 # Module Imports
-Import-Module oh-my-posh
+Import-Module PSReadLine
 
-# Shell Settings
-oh-my-posh init pwsh --config "${HOME}\AppData\Local\Programs\oh-my-posh\themes\derpenstiltskin.omp.json" | Invoke-Expression
+#PSReadLine Config
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle InlineView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -HistorySaveStyle SaveNothing
+
+# starship.rs
+Invoke-Expression (&starship init powershell)
